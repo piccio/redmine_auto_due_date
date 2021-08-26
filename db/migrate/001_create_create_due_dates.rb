a731fa2477531130cc7e69e6af3f5d7f199910c9
Roberto Piccini <roberto.piccini@gmail.com>
2021-08-26 10:23:03 +0200

    bugfix: migration versioning

--- db/migrate/001_create_create_due_dates.rb
+++ db/migrate/001_create_create_due_dates.rb
@@ -1,4 +1,4 @@
-class CreateCreateDueDates < ActiveRecord::Migration
+class CreateCreateDueDates < ActiveRecord::Migration[4.2]
   def change
     create_table :create_due_dates do |t|
       t.references :project, null: false, index: { unique: true }
--- db/migrate/002_create_update_due_dates.rb
+++ db/migrate/002_create_update_due_dates.rb
@@ -1,4 +1,4 @@
-class CreateUpdateDueDates < ActiveRecord::Migration
+class CreateUpdateDueDates < ActiveRecord::Migration[4.2]
   def change
     create_table :update_due_dates do |t|
       t.references :project, null: false, index: { unique: true }
